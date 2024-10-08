c555d49214fc467e1fee8f79e5fd83ad20e207a8
Jove Beckett <jovebeckett2@gmail.com>
2024-10-07 22:08:29 -0700

    newest

--- build.jov.sh
+++ build.jov.sh
@@ -1,5 +1,7 @@
+set -e
 # gcc ./test.c -Ilua/src -Llua/src -llua -lm -o test
-#
-bear -- g++  -o main ./main.cpp -L. -ljovial_engine -I../NewJovialEngine/src -Ilua/src -Llua/src -llua  -ggdb
-#
-# g++ ./main.cpp -Ilua/src -Llua/src -llua -L. -ljovial_engine -I../NewJovialEngine/src -ggdb -o main -Wl,--trace
+
+# bear -- g++  -o main ./main.cpp -L. -ljovial_engine -I../NewJovialEngine/src -Ilua/src -Llua/src -llua  -ggdb
+
+g++ -o editor/editor ./editor/editor.cpp -L. -ljovial_engine -I../NewJovialEngine/src -Ilua/src -Llua/src -llua -ggdb -lfreetype -fsanitize=address,undefined
+./editor/editor
